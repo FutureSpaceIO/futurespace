75f023cc6decb1a00ee54d5aac4ba1dbae87068d
fundon <cfddream@gmail.com>
2015-11-14 15:10:01 +0800

    improve homepage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,42 +1,8 @@
-var FS = {
-  hasClass: function(obj, cls) {
-    return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
-  },
-
-  addClass: function(obj, cls) {
-    if (!this.hasClass(obj, cls)) obj.className += " " + cls;
-  },
-
-  removeClass: function(obj, cls) {
-    if (this.hasClass(obj, cls)) {
-      var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-      obj.className = obj.className.replace(reg, ' ');
-    }
-  },
-
-  toggleClass: function(obj, cls) {
-    if (this.hasClass(obj, cls)) {
-      this.removeClass(obj, cls);
-    } else {
-      this.addClass(obj, cls);
-    }
-  },
-
-  bindEvents: function() {
-    var that = this;
-    var toggleMenu = document.querySelector(".nav-toggle");
-    toggleMenu.addEventListener("touchstart", function(e) {
-      that.toggleClass(toggleMenu, "active");
-    }, false);
-  },
-
-  init: function() {
-    this.bindEvents();
-  }
-};
-
 (function(doc) {
   doc.addEventListener("DOMContentLoaded", function() {
-    FS.init();
+    var menu = doc.querySelector('.nav-toggle');
+    menu.addEventListener('click', function(e) {
+      this.classList.toggle('active');
+    }, false);
   }, false);
 })(document);
