2e6016232f20ba154cf8d689b18f2439bc27d9cf
fundon <cfddream@gmail.com>
2015-11-23 23:02:46 +0800

    add app.keys

--- server.js
+++ server.js
@@ -3,8 +3,9 @@
 import Trek from 'trek'
 
 const app = new Trek(__dirname)
+app.keys = ['futurespace.io', 'stackmeta.com']
 
-app.on('error', (err, context) => {
+app.on('error', (err, ctx) => {
   app.logger.error(err)
 })
 
